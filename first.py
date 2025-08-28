90ca2b7cd0d5180c0ff5d386a4c772d7f0e9cab2
diwakar998 <34593700+diwakar998@users.noreply.github.com>
2025-08-28 12:21:42 +0530

    Update first.py

--- first.py
+++ first.py
@@ -2,9 +2,10 @@ pip install -U langchain langchain-openai
 
 LANGSMITH_TRACING="true"
 LANGSMITH_ENDPOINT="https://api.smith.langchain.com"
-LANGSMITH_API_KEY="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"
+LANGSMITH_API_KEY=st.secrets["LANGSMITH_API_KEY"]
+#"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"
 LANGSMITH_PROJECT="firstLangAgent"
-openai.api_key = st.secrets["OPENAI_API_KEY"]
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 #OPENAI_API_KEY="<your-openai-api-key>"
 
 from langchain_openai import ChatOpenAI
